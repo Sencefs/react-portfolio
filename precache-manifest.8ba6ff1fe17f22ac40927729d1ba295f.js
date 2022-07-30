04ce2769606c9190a8dec81893131e37f2fb4212
Sencefs <100245563+Sencefs@users.noreply.github.com>
2022-07-30 15:01:20 -0400

    Updates

---
+++ precache-manifest.8ba6ff1fe17f22ac40927729d1ba295f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbdb888a5b087a7aad1d",
+    "url": "/react-portfolio/static/css/main.a4f7cda1.chunk.css"
+  },
+  {
+    "revision": "fbdb888a5b087a7aad1d",
+    "url": "/react-portfolio/static/js/main.fbdb888a.chunk.js"
+  },
+  {
+    "revision": "598671ac997d37cdc880",
+    "url": "/react-portfolio/static/js/1.598671ac.chunk.js"
+  },
+  {
+    "revision": "7f931c62e788facba93d",
+    "url": "/react-portfolio/static/js/2.7f931c62.chunk.js"
+  },
+  {
+    "revision": "54af89ee7b9bc7a8085a",
+    "url": "/react-portfolio/static/js/runtime~main.54af89ee.js"
+  },
+  {
+    "revision": "6c9af340d7cbc3115416240ce04d0443",
+    "url": "/react-portfolio/static/media/placeholder_1_600.6c9af340.jpg"
+  },
+  {
+    "revision": "8e0d5f95e3f2f46f25730c030c2007b0",
+    "url": "/react-portfolio/static/media/placeholder_2_600.8e0d5f95.jpg"
+  },
+  {
+    "revision": "d5888a70d7a24e4e251f46015558fece",
+    "url": "/react-portfolio/static/media/placeholder_3_600.d5888a70.jpg"
+  },
+  {
+    "revision": "95957f978cc506e6a6e07e99f11e7752",
+    "url": "/react-portfolio/static/media/note-taker.95957f97.jpg"
+  },
+  {
+    "revision": "9f6f2f2b40b508c8456b210b3b3df578",
+    "url": "/react-portfolio/static/media/weatherdashboard.9f6f2f2b.jpg"
+  },
+  {
+    "revision": "23184a31e05c13d28d23d6a0810af3c2",
+    "url": "/react-portfolio/static/media/project_1.23184a31.jpg"
+  },
+  {
+    "revision": "8399a54a3ab390deaee21085c17e56b2",
+    "url": "/react-portfolio/index.html"
+  }
+];
